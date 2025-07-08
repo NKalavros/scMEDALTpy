d8f06960a446881fb50d2192106f4e7efcd2cb56
UshiromiayDev <kalavrosnikolaos@gmail.com>
2025-07-08 12:44:04 -0400

    Moving some files along

--- example/run.example.sh
+++
@@ -1,5 +0,0 @@
-python scTree.py -P ./ -I ./example/scDNA.CNV.txt -O ./example/outputDNA -D D -G hg19
-python scTree.py -P ./ -I ./example/scRNA.CNV.txt -O ./example/outputRNA -D R -G hg19
-
-python scTree.py -P ./ -I ./example/scDNA.CNV.txt -O ./example/outputDNAT -D D -R T -G hg19
-python scTree.py -P ./ -I ./example/scRNA.CNV.txt -O ./example/outputRNAT -D R -R T -G hg19
